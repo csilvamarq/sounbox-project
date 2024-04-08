d220f3d24f58eb6422af0e841009ccb0360048be
csilvamarq <csilvamarq@ceit.es>
2024-04-08 07:45:48 +0200

    (UPDATE) migrated translator

--- index.py
+++ index.py
@@ -1,13 +1,10 @@
 from transcriber import transcriber
-from translator import translator
 from voiceBox import voicebox
 from player import player
 from threading import Thread
 t1 = Thread(target=transcriber)
-t2 = Thread(target=translator)
 t3 = Thread(target=voicebox)
 t4 = Thread(target=player)
 t1.start()
-t2.start()
 t3.start()
 t4.start()
